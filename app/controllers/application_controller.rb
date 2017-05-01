bb48aab8989b0fab860542292b326b11dcdb047f
robertjosephwayne <robertjwayne@gmail.com>
2017-05-01 15:03:10 -0400

    Add get '/' and post '/teams' routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,22 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :super_hero
+  end
+
+  post '/teams' do
+    @team = params[:team]
+
+    params[:team][:members].each do |member_info|
+      SuperHero.new(member_info)
+    end
+
+    @superheroes = SuperHero.all
+
+    erb :team
+  end
 
 end
